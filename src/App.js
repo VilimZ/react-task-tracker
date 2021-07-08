c7a912535affca559508a5e91ea15e6145a9ed6e
Vilim <vilim.zovko@gmail.com>
2021-07-08 19:11:14 +0200

    Added task.js and tasks.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Header from './components/Header';
-
+import Tasks from './components/Tasks';
+import { useState } from "react"
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctor Apointment',
+        day: 'Jul 6th at 3:15 am',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'School Investigation',
+        day: 'Apr 11th at 2:30 pm',
+        reminder: true,
+    },    {
+        id: 3,
+        text: 'Construction Start',
+        day: 'Jul 6th at 5:45 am',
+        reminder: true,
+    },
+    
+    
+])
+
+//Delete Task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !==id))
+}
   return (
    <div className="container">
       <Header />
+      <Tasks tasks={  tasks  } onDelete = {deleteTask}/>
       </div>
    )
     
